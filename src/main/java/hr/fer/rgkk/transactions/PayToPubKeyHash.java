86cc672c6393f2171711df7353fba17cd6ef5e38
valentin <valentin.vuky@gmail.com>
2022-11-10 18:50:38 +0100

    Add first assignment

--- src/main/java/hr/fer/rgkk/transactions/PayToPubKeyHash.java
+++ src/main/java/hr/fer/rgkk/transactions/PayToPubKeyHash.java
@@ -1,14 +1,14 @@
 package hr.fer.rgkk.transactions;
 
 import org.bitcoinj.core.*;
-import org.bitcoinj.core.Utils;
-import org.bitcoinj.crypto.TransactionSignature;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
 
 import static org.bitcoinj.script.ScriptOpCodes.*;
 
 public class PayToPubKeyHash extends ScriptTransaction {
+
+    private final ECKey ecKey = new ECKey();
 
     public PayToPubKeyHash(WalletKit walletKit, NetworkParameters parameters) {
         super(walletKit, parameters);
@@ -17,12 +17,30 @@ public class PayToPubKeyHash extends ScriptTransaction {
     @Override
     public Script createLockingScript() {
         // TODO: Create Locking script
-        throw new UnsupportedOperationException();
+        try {
+            return new ScriptBuilder()
+                    .data(ecKey.getPubKey())
+                    .op(OP_DUP)
+                    .op(OP_HASH160)
+                    .data(ecKey.getPubKeyHash())
+                    .op(OP_EQUALVERIFY)
+                    .op(OP_CHECKSIG)
+                    .build();
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+
     }
 
     @Override
     public Script createUnlockingScript(Transaction unsignedTransaction) {
-        // TODO: Create Unlocking script
-        throw new UnsupportedOperationException();
+        try {
+            return new ScriptBuilder()
+                    .data(sign(unsignedTransaction, ecKey).encodeToBitcoin())
+                    .build();
+        } catch (Exception e) {
+            throw new UnsupportedOperationException();
+        }
+
     }
 }
